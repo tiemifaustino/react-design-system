e73265e22b9d6397a71fe449c92f84b257ee064f
Tiemi Faustino <hayashipharma@gmail.com>
2023-08-29 22:38:36 -0300

    feat: :sparkles: Create Text component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,9 +1,57 @@
-import { Text } from "./Text";
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from "./Text";
 
+// configurações globais para todas as variações
 export default {
   title: 'Components/Text',
   component: Text,
-}
+  args: {
+    children: 'Lorem ipsum',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      }
+    }
+  }
+} as Meta<TextProps>
 
 // precisa exportar uma variável
-export const Default = {}
+export const Default: StoryObj<TextProps> = {}
+
+// configurações por variação
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Text with P tag</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    }
+  }
+}
